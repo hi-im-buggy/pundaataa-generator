ecf26c3ec11ff52ad9c9926218da0615589b30f2
Pratyaksh Gautam <pratyaksh69@gmail.com>
2020-02-07 13:12:00 +0530

    added text wrapping

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
+from textwrap import TextWrapper
 
 img = Image.open("src/template.jpg")
 #open the image as the object img
@@ -10,11 +11,13 @@ selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
 width, height = img.size
 x_begin, y_begin = width/10, height/5       #need a bit more space vertically to account for header.
 
+char_width = 22                     #the number of characters allowed per line
+wrapper = TextWrapper(width = char_width)
 print("Enter the text which you want to be input - ")
-input_text = input()
+formatted_text = wrapper.fill(input())
 draw.text(
             (x_begin, y_begin),         #beginning coordinates
-            input_text,                 #input text
+            formatted_text,             #input text
             (218, 243, 239),            #rgb values for text colour
             font = selected_font        #font selection
         )
