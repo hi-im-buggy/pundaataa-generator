8854e61e6afb6413dd444327f984ef901a99822a
Pratyaksh Gautam <pratyaksh69@gmail.com>
2020-02-08 10:26:41 +0530

    added code to get the max line width of the rendered text

--- generate.py
+++ generate.py
@@ -1,20 +1,33 @@
 from PIL import Image, ImageFont, ImageDraw
 from textwrap import TextWrapper
+from operator import itemgetter
 
 img = Image.open("src/template.jpg")
 #open the image as the object img
 draw = ImageDraw.Draw(img)
 #create object draw, whose member function will be used to draw the actual text
-
-fontsize = 100
-selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
 width, height = img.size
 x_begin, y_begin = width/10, height/5       #need a bit more space vertically to account for header.
 
 char_width = 22                     #the number of characters allowed per line
+fontsize = 100
+
 wrapper = TextWrapper(width = char_width)
+selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
+
 print("Enter the text which you want to be input - ")
-formatted_text = wrapper.fill(input())
+input_text = input()
+wrapped_lines = wrapper.wrap(input_text)            #lines wrapped and separated, and put into a list
+formatted_text = wrapper.fill(input_text)           #lines wrapped and concatenated into a single string with newline characters
+line_width_list = []
+
+for line in wrapped_lines:
+    line_width_list.append( selected_font.getsize(line) )   #initialize a list of sizes for the rendered lines in our selected font
+
+max_line_width = max(line_width_list, key = itemgetter(0))[0]       #return the max width of rendered line
+print(wrapped_lines)
+print(max_line_width)
+
 draw.text(
             (x_begin, y_begin),         #beginning coordinates
             formatted_text,             #input text
