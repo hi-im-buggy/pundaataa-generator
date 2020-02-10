d3c33358f962bff806044d45e146c0c0b630cd9f
Pratyaksh Gautam <pratyaksh69@gmail.com>
2020-02-10 09:12:02 +0530

    Almost got it

--- generate.py
+++ generate.py
@@ -2,17 +2,17 @@ from PIL import Image, ImageFont, ImageDraw
 from textwrap import TextWrapper
 from operator import itemgetter
 
+char_width = 30                 #the number of characters allowed per line
+fontsize = 70
+text_box_height = 0
+text_box_width = 0
+
 img = Image.open("src/template.jpg")
 #open the image as the object img
 draw = ImageDraw.Draw(img)
 #create object draw, whose member function will be used to draw the actual text
 width, height = img.size
-x_begin, y_begin = width/2 , (height/2 + 30)     #need a bit more space vertically to account for header.
-
-char_width = 22                             #the number of characters allowed per line
-fontsize = 100
-text_box_height = 0
-text_box_width = 0
+x_begin, y_begin = width/2 , height/2     #need a bit more space vertically to account for header.
 
 wrapper = TextWrapper(width = char_width)
 selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
@@ -29,16 +29,60 @@ for line in wrapped_lines:
 
 text_box_width = max(line_size_list, key = itemgetter(0))[0]       #return the max width of rendered line i.e. the text box width
 
+#increase the font size till width is maximised
+while text_box_width < 0.8 * width:
+    fontsize += 2
+    selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
+    for line in wrapped_lines:
+        line_size_list.append( selected_font.getsize(line) )   
+    text_box_width = max(line_size_list, key = itemgetter(0))[0]       
+
 for line in line_size_list:                 #text box height will simply be the sum of the heights of the rendered lines
     text_box_height += line[1]
 
+#Now the text box width has been maximised, but the text box height may still be too much
+if len(wrapped_lines) > 1:
+    while text_box_height > 0.6 * height:
+        fontsize -= 2
+        selected_font = ImageFont.truetype("src/CaviarDreams.ttf", fontsize)
+        line_size_list.clear()
+        for line in wrapped_lines:
+            line_size_list.append( selected_font.getsize(line) )   
+        text_box_width = max(line_size_list, key = itemgetter(0))[0]       
+        #we reduce the font size, and then get the text box width with the new font size
+        #then we maximise the text box width again, this time by increasing number of characters per line instead
+        while text_box_width < 0.8 * width:
+            char_width += 10
+            wrapper.width = char_width
+            wrapped_lines = wrapper.wrap(input_text)
+            line_size_list.clear()
+            for line in wrapped_lines:
+                line_size_list.append( selected_font.getsize(line) )
+            text_box_width = max(line_size_list, key = itemgetter(0))[0]       
+        text_box_height = 0
+        for line in line_size_list:                 #text box height will simply be the sum of the heights of the rendered lines
+            text_box_height += line[1]
+                
+#Get the final updates text box dimensions so as to calculate coordinates for centering
+#but it doesn't fucking work for long paragraphs and just prints off center so fuck me right?
+wrapped_lines = wrapper.wrap(input_text)
+line_size_list.clear()
+for line in wrapped_lines:
+    line_size_list.append( selected_font.getsize(line) )
+text_box_width = max(line_size_list, key = itemgetter(0))[0]       
+text_box_height = 0
+for line in line_size_list:                 #text box height will simply be the sum of the heights of the rendered lines
+    text_box_height += line[1]
+     
+#Offset the x and y beginning coordinates to center the text
 x_begin -= (text_box_width/2)
 y_begin -= (text_box_height/2)
-
+#There's some sort of bullshit problem going on with text_box_height variable, really short input text, it gives me a huge offset,
+#and makes the whole text disappear off of the face of the image.
 draw.text(
             (x_begin, y_begin),         #beginning coordinates
             formatted_text,             #input text
-            (218, 243, 239),            #rgb values for text colour
+            (218, 243, 239),            #RGB values for text colour
             font = selected_font        #font selection
         )
 img.save("output.jpg")
